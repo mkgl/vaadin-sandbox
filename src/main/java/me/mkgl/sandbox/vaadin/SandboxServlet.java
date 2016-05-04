07b72f23e23ecda04ba6eab57ef31e3aa38e4790
Mikaël Geljić <m.geljic@gmail.com>
2016-05-04 23:58:35 +0200

    Vaadin Sandbox POM and headless servlet
    * handles all requests at /VAADIN/*
    * leave other UIs and servlets independent

---
+++ src/main/java/me/mkgl/sandbox/vaadin/SandboxServlet.java
@@ -0,0 +1,12 @@
+package me.mkgl.sandbox.vaadin;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+@WebServlet(value = "/VAADIN/*", asyncSupported = true)
+@VaadinServletConfiguration(productionMode = false, ui = UI.class)
+public class SandboxServlet extends VaadinServlet {
+}
